5ad6e9b097c23bbe0a7757462e95d1becef5e70a
lovejoy <personalovejoy@gmail.com>
2017-09-26 15:21:11 -0400

    change shell script

--- MaxPrimeGapByCpp/build.sh
+++ MaxPrimeGapByCpp/build.sh
@@ -1,3 +1,3 @@
 mpic++ ./MaxPrimeGap.cpp -o MaxPrimeGap -lgmp
 
-sqsub -q mpi -r 3d -o ofile_9_8.txt  -n 8 ./MaxPrimeGap 1000000000
+sqsub -q mpi -r 3d -o ofile_9_8.log  -n 8 ./MaxPrimeGap 1000000000
