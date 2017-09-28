c28e6881a9803274e74e6279d8a51b3d0869cbeb
lovejoy <personalovejoy@gmail.com>
2017-09-27 22:41:21 -0400

    use unsigned long long instead of long long

--- MaxPrimeGapByCpp/MaxPrimeGap.cpp
+++ MaxPrimeGapByCpp/MaxPrimeGap.cpp
@@ -16,8 +16,8 @@ using namespace std;
 
 // check if the result is correct or not -> https://en.wikipedia.org/wiki/Prime_gap
 
-long long mpz_get_ll(mpz_t t);
-void mpz_set_ll(mpz_t t, long long l);
+unsigned long long mpz_get_ull(mpz_t t);
+void mpz_set_ull(mpz_t t, unsigned long long l);
 
 int main(int args, char **argv)
 {
@@ -25,7 +25,7 @@ int main(int args, char **argv)
     int pro_rank; // rank of current process
     double start_time, end_time;
     MPI_Status status;
-    long long upper_limit;
+    unsigned long long upper_limit;
 
     if (args > 1)
     {
@@ -44,34 +44,34 @@ int main(int args, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &pro_rank);
     start_time = MPI_Wtime();
 
-    long long quotient = upper_limit / pro_size; // e.g. q=3 u=20 p=6
+    unsigned long long quotient = upper_limit / pro_size; // e.g. q=3 u=20 p=6
     int remainder = upper_limit % pro_size; // e.g. r=2
     int shift_sum = pro_rank < remainder ? pro_rank : remainder;
     int shift = pro_rank < remainder ? 1 : 0;
-    long long start = pro_rank * quotient + shift_sum + 1; // first value of current process
-    long long next_start = start + quotient + shift; // first value of next process if it exists
+    unsigned long long start = pro_rank * quotient + shift_sum + 1; // first value of current process
+    unsigned long long next_start = start + quotient + shift; // first value of next process if it exists
 
-    long long curr, next_prime, pre_prime, left_prime, right_prime, max_gap, gap;
+    unsigned long long curr, next_prime, pre_prime, left_prime, right_prime, max_gap, gap;
     mpz_t gmp_curr, gmp_next_prime;
     mpz_init(gmp_curr);
     mpz_init(gmp_next_prime);
     for (curr = start, pre_prime = 0; curr < next_start && curr != pre_prime; curr = next_prime)
     {
-        if (curr !=start) // each process will calc the gap of the end,
+        if (curr != start) // each process will calc the gap of the end
         {
             pre_prime = next_prime; // ignore the gap at the beginning
         }
 
-        mpz_set_ll(gmp_curr, curr);
+        mpz_set_ull(gmp_curr, curr);
         mpz_nextprime(gmp_next_prime, gmp_curr); // gmp build-in method
-        next_prime = mpz_get_ll(gmp_next_prime);
+        next_prime = mpz_get_ull(gmp_next_prime);
 
         if (next_prime > upper_limit) // if it exceed the upper limit
         {
             next_prime = pre_prime;
         }
 
-        if (curr !=start) // calc gap for each 2 prime numbers
+        if (curr != start) // calc gap for each 2 prime numbers
         {
             gap = next_prime - pre_prime;
         }
@@ -87,9 +87,9 @@ int main(int args, char **argv)
 
     if (pro_rank == 0)
     {
-        long long global_max_gap = max_gap; // get results from itself
-        long long global_left_prime = left_prime;
-        long long global_right_prime = right_prime;
+        unsigned long long global_max_gap = max_gap; // get results from itself
+        unsigned long long global_left_prime = left_prime;
+        unsigned long long global_right_prime = right_prime;
 
         for (int i = 1; i < pro_size; i++) // update them with data from other processes
         {
@@ -122,14 +122,14 @@ int main(int args, char **argv)
     return 0;
 }
 
-long long mpz_get_ll(mpz_t t)
+unsigned long long mpz_get_ull(mpz_t t)
 {
-    long long val = 0;
+    unsigned long long val = 0;
     mpz_export(&val, 0, -1, sizeof val, 0, 0, t);
     return val;
 }
 
-void mpz_set_ll(mpz_t t, long long l)
+void mpz_set_ull(mpz_t t, unsigned long long l)
 {
     mpz_import(t, 1, -1, sizeof l, 0, 0, &l);
 }
